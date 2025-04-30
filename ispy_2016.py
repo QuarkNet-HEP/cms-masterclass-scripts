ed538a1dbf97f7b8ea8c1c108d62bb672b511186
mccauley <thomas.mccauley@cern.ch>
2025-04-30 18:42:08 +0200

    doubleeg2016g

--- ispy_2016.py
+++ ispy_2016.py
@@ -16,8 +16,8 @@ prefix = 'root://eospublic.cern.ch//eos/opendata/cms'
 
 import FWCore.Utilities.FileUtils as FileUtils
 
-files = FileUtils.loadListFromFile("DoubleMu_2016G.txt")
-#files = FileUtils.loadListFromFile("DoubleEG_2016G.txt")
+#files = FileUtils.loadListFromFile("DoubleMu_2016G.txt")
+files = FileUtils.loadListFromFile("DoubleEG_2016G.txt")
 #files = FileUtils.loadListFromFile("DoubleMu_2016H.txt")
 #files = FileUtils.loadListFromFile("DoubleEG_2016H.txt")
 
@@ -33,7 +33,7 @@ process.source = cms.Source(
 
 process.add_(
     cms.Service("ISpyService",
-    outputFileName = cms.untracked.string('DoubleMu2016G.ig'),
+    outputFileName = cms.untracked.string('DoubleEG2016G.ig'),
     outputMaxEvents = cms.untracked.int32(50)
     )
 )
