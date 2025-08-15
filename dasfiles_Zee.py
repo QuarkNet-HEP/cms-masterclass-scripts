b3471ef47780b3cb07d68cc207a55ec76229e7ca
mccauley <thomas.mccauley@cern.ch>
2025-08-15 16:20:11 +0200

    Zee and Zmumu

---
+++ dasfiles_Zee.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+def make_run_lumi(df):
+    return [f'{r}:{l}' for r,l in zip(df['Run'].tolist(), df['Lumi'].tolist())]
+
+command = "/cvmfs/cms.cern.ch/common/dasgoclient"
+
+inputs = [
+    {
+        'name': 'DoubleElectronRun2016H', 
+        'run_lumi': make_run_lumi(pd.read_csv('csv/DoubleElectronRun2016H.csv')),
+        'dataset': '/DoubleEG/Run2016H-UL2016_MiniAODv2-v1/MINIAOD'
+    }
+]
+
+for i in inputs:
+
+    name = i['name']
+    dasfile = open(f'dasfile_{name}.sh', 'w')
+
+    run_lumi = i['run_lumi']
+    dataset = i['dataset']
+
+    # Use a set to get rid of duplicates
+    run_lumi = list(set(run_lumi))
+    
+    for rel in run_lumi:
+
+        run = rel.split(':')[0]
+        lumi = rel.split(':')[1]
+        query = f'{command} -query="file dataset={dataset} run={run} lumi={lumi}"\n'
+        dasfile.write(query)
+
+    dasfile.close()
+
---
+++ ispy_twolepton.py
@@ -0,0 +1,92 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Eras.Era_Run2_2018_cff import Run2_2018
+from FWCore.MessageLogger.MessageLogger_cfi import *
+
+process = cms.Process("process", Run2_2018)
+
+process.load('Configuration.StandardSequences.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_data', '')
+
+# Note: this will replace /store/data in file name
+prefix = 'root://eospublic.cern.ch//eos/opendata/cms'
+
+import FWCore.Utilities.FileUtils as FileUtils
+
+files = FileUtils.loadListFromFile("DoubleMuonRun2016H.txt")
+#files = FileUtils.loadListFromFile("DoubleElectronRun2016H.txt")
+output_file_name = 'DoubleMuon2016H.ig'
+#output_file_name = 'DoubleElectron2016H.ig'
+
+newfiles = [f.replace('/store/data', prefix) for f in files]
+
+run_events = FileUtils.loadListFromFile("run_event_twolepton.txt")
+
+process.source = cms.Source(
+    "PoolSource",
+    fileNames = cms.untracked.vstring(*newfiles),
+    eventsToProcess = cms.untracked.VEventRange(*run_events)
+)
+
+process.add_(
+    cms.Service("ISpyService",
+    outputFileName = cms.untracked.string(output_file_name),
+    outputMaxEvents = cms.untracked.int32(50)
+    )
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(100)
+)
+
+process.load("ISpy.Analyzers.ISpyEvent_cfi")
+process.load('ISpy.Analyzers.ISpyEBRecHit_cfi')
+process.load('ISpy.Analyzers.ISpyEERecHit_cfi')
+process.load('ISpy.Analyzers.ISpyESRecHit_cfi')
+process.load('ISpy.Analyzers.ISpyHBRecHit_cfi')
+process.load('ISpy.Analyzers.ISpyHERecHit_cfi')
+process.load('ISpy.Analyzers.ISpyHFRecHit_cfi')
+process.load('ISpy.Analyzers.ISpyHORecHit_cfi')
+process.load('ISpy.Analyzers.ISpyPATMuon_cfi')
+process.load('ISpy.Analyzers.ISpyPATElectron_cfi')
+process.load('ISpy.Analyzers.ISpyPackedCandidate_cfi')
+process.load('ISpy.Analyzers.ISpyVertex_cfi')
+process.load('ISpy.Analyzers.ISpyPATJet_cfi')
+process.load('ISpy.Analyzers.ISpyPATMET_cfi')
+
+process.ISpyEBRecHit.iSpyEBRecHitTag = cms.InputTag('reducedEgamma:reducedEBRecHits')
+process.ISpyEERecHit.iSpyEERecHitTag = cms.InputTag('reducedEgamma:reducedEERecHits')
+process.ISpyESRecHit.iSpyESRecHitTag = cms.InputTag('reducedEgamma:reducedESRecHits')
+process.ISpyHBRecHit.iSpyHBRecHitTag = cms.InputTag("reducedEgamma:reducedHBHEHits")
+process.ISpyHERecHit.iSpyHERecHitTag = cms.InputTag("reducedEgamma:reducedHBHEHits")
+process.ISpyHFRecHit.iSpyHFRecHitTag = cms.InputTag("slimmedHcalRecHits:reducedHcalRecHits")
+process.ISpyHORecHit.iSpyHORecHitTag = cms.InputTag("slimmedHcalRecHits:reducedHcalRecHits")
+process.ISpyPATMuon.iSpyPATMuonTag = cms.InputTag("slimmedMuons")
+process.ISpyPATElectron.iSpyPATElectronTag = cms.InputTag("slimmedElectrons")
+process.ISpyPackedCandidate.iSpyPackedCandidateTag = cms.InputTag('packedPFCandidates')
+process.ISpyPATJet.iSpyPATJetTag = cms.InputTag('slimmedJets')
+process.ISpyPATMET.iSpyPATMETTag = cms.InputTag('slimmedMETs')
+process.ISpyVertex.iSpyPriVertexTag = cms.InputTag('offlineSlimmedPrimaryVertices')
+
+process.iSpy = cms.Path(process.ISpyEvent*
+                        process.ISpyEBRecHit*
+                        process.ISpyEERecHit*
+                        process.ISpyESRecHit*
+                        process.ISpyHBRecHit*
+                        process.ISpyHERecHit*
+                        process.ISpyHFRecHit*
+                        process.ISpyHORecHit*
+                        process.ISpyPATMuon*
+                        process.ISpyPATElectron*
+                        process.ISpyPATJet*
+                        process.ISpyPATMET*
+                        process.ISpyPackedCandidate*
+                        process.ISpyVertex)
+
+process.schedule = cms.Schedule(process.iSpy)
+
+
+
